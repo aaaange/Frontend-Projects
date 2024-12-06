3539784491d5944474af69dfe438b957aa9c7528
aaaange <ayung00@naver.com>
2024-12-06 23:22:43 +0900

    Vanilla JS day05

--- Vanilla JS/Study_code/app.js
+++ Vanilla JS/Study_code/app.js
@@ -1,3 +1,54 @@
+// 2.13 Conditonals
+// const age = prompt("How old are you?");
+
+// console.log(typeof age, parseInt(age)) ; //typeof 변수를 쓰면 변수의 타입을 출력
+
+
+// const age = parseInt(prompt("How old are you?"));
+
+// console.log(age);
+
+
+
+
+// 2.14 Conditionals part Two
+
+// const age = parseInt(prompt("How old are you?"));
+
+// console.log(isNaN(age));
+
+// if(condition){
+//     // condition === ture
+// }
+
+// if(isNaN(age)){
+//     console.log('Please write a number')
+// } else {
+//     console.log('Thank you for wirting your age.')
+// }
+
+
+
+// 2.15 Conditionals part Three
+
+// const age = parseInt(prompt("How old are you?"));
+
+// if(isNaN(age)){
+//     console.log('Please write a number');
+// } else if (age < 18) {
+//     console.log("You are too young.");
+// } else if (age >= 18 && age <= 50) {
+//     console.log("You can drink.");
+// } else if (age > 50 && age <=80) {
+//     console.log("You should exercise");
+// } else if (age > 80) {
+//     console.log("You can do whatever you want")
+// }
+
+
+
+// 3.1 
+
 // const title = document.getElementById("title");
 
 // // console.log('title')
@@ -65,44 +116,103 @@
 
 // 3.5 More Events
 
+// const h1 = document.querySelector('div.hello:first-child h1');
+
+// function handleTitleClick() {
+//     console.log('title was clicked');
+//     h1.style.color = 'blue';
+// }
+
+// function handleMouseEnter() {
+//     console.log('mouse is here!')
+//     h1.innerText = "Mouse is here!"
+// }
+
+// function handleMouseLeave() {
+//     h1.innerText = 'Mouse is gone!';
+// }
+
+// function handleWindowResize() {
+//     document.body.style.backgroundColor = 'tomato'; //여기서 bady는 중요한 부분
+// }
+
+// function handleWindowCopy() {
+//     alert('copoer!');
+// }
+
+// function handlewindowOffline() {
+//     alert('SOS no WIFI');
+// }
+
+// function handleWindowOnline() {
+//     alert('ALL GOOOOOOD')
+// }
+
+// h1.onclick = handleTitleClick; 
+// h1.addEventListener('mouseenter', handleMouseEnter);
+// h1.addEventListener('mouseleave', handleMouseLeave);
+// // 이벤트 리스너를 작성하는 방법은 위와 같이 2종류
+
+// window.addEventListener('resize', handleWindowResize);
+// window.addEventListener('copy', handleWindowCopy);
+// window.addEventListener('offline', handlewindowOffline);
+// window.addEventListener('online', handleWindowOnline);
+
+
+
+// 3.6 CSS in JavaScirpt
+// const h1 = document.querySelector('div.hello:first-child h1');
+
+// function handleTitleClick() {
+//     const currrentColor = h1.style.color;
+//     let newColor;
+//     if(currrentColor === 'blue') {
+//         newColor = 'tomato';
+//     } else {
+//         newColor = 'blue';
+//     }
+//     h1.style.color = newColor;
+// }
+
+// h1.onclick = handleTitleClick; 
+
+
+
+
+// 3.7CSS in JavaScript part Two
+// const h1 = document.querySelector('div.hello:first-child h1');
+
+// function handleTitleClick() {
+//     const clickedClass = 'clicked';
+//     if (h1.className === 'clickedClass') {
+//         h1.className  = "";
+//     } else {
+//         h1.className = 'clickedClass';
+//     }
+// }
+
+// h1.onclick = handleTitleClick; 
+
+
+
+
+// 3.8 CSS in JavaScript part Three
+// className을 사용하면 이전에 있던 클래스는 고려하지 않고 교체함
+// classList는 조금 다름
+
 const h1 = document.querySelector('div.hello:first-child h1');
 
+// function handleTitleClick() {
+//     const clickedClass = 'clicked';
+//     if (h1.classList.contains(clickedClass) === 'clickedClass') {
+//         h1.classList.remove(clickedClass);
+//     } else {
+//         h1.classList.add(clickedClass);
+//     }
+// }
+
 function handleTitleClick() {
-    console.log('title was clicked');
-    h1.style.color = 'blue';
-}
-
-function handleMouseEnter() {
-    console.log('mouse is here!')
-    h1.innerText = "Mouse is here!"
-}
-
-function handleMouseLeave() {
-    h1.innerText = 'Mouse is gone!';
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = 'tomato'; //여기서 bady는 중요한 부분
-}
-
-function handleWindowCopy() {
-    alert('copoer!');
-}
-
-function handlewindowOffline() {
-    alert('SOS no WIFI');
-}
-
-function handleWindowOnline() {
-    alert('ALL GOOOOOOD')
+    h1.classList.toggle("clicked");
 }
 
 h1.onclick = handleTitleClick; 
-h1.addEventListener('mouseenter', handleMouseEnter);
-h1.addEventListener('mouseleave', handleMouseLeave);
-// 이벤트 리스너를 작성하는 방법은 위와 같이 2종류
-
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy', handleWindowCopy);
-window.addEventListener('offline', handlewindowOffline)₩;
-window.addEventListener('online', handleWindowOnline);
