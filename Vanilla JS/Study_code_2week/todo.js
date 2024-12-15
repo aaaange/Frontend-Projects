276139bc969ea5649de3305de9a2d8aef36d8436
aaaange <ayung00@naver.com>
2024-12-15 19:50:11 +0900

    Vanilla JS day 14 - Delete todo

--- Vanilla JS/Study_code_2week/todo.js
+++ Vanilla JS/Study_code_2week/todo.js
@@ -13,7 +13,34 @@
 // toDoForm.addEventListener("submit", handleToDoSubmit) 
 
 
+
 // 7.1 Adding ToDos
+// const toDoForm = document.getElementById("todo-form");
+// const toDoInput = toDoForm.querySelector("input");
+// const toDoList = document.getElementById("todo-list");
+
+// function paintToDo(newTodo) {
+//     const li = document.createElement("li")
+//     // 변수명은 li라고 지을 필요 없이 원하는 대로 지으면 됨.
+//     // 그러나 document.createElement("li") 이 안에 작성되는 태그 이름은 꼭 지켜줘야 함.
+//     const span = document.createElement("span");
+//     li.appendChild(span); // span을 li 안에 넣어주기.
+//     span.innerText = newTodo;
+//     toDoList.appendChild(li);
+// }
+
+// function handleToDoSubmit(event) {
+//     event.preventDefault();
+//     const newTodo = toDoInput.value;
+//     toDoInput.value = "";
+//     paintToDo(newTodo);
+// }
+
+// toDoForm.addEventListener("submit", handleToDoSubmit) 
+
+
+
+// 7.2 Deleting To Dos
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
