92c2985fc9319ff70564b76deb108542c583ff67
aaaange <ayung00@naver.com>
2024-12-16 00:38:03 +0900

    Vanilla JS day 14 - Loading Todos 1

--- Vanilla JS/Study_code_2week/todo.js
+++ Vanilla JS/Study_code_2week/todo.js
@@ -75,17 +75,61 @@
 
 
 // 7.3 Saving To Dos
+// const toDoForm = document.getElementById("todo-form");
+// const toDoInput = toDoForm.querySelector("input");
+// const toDoList = document.getElementById("todo-list");
+
+// const toDos = [];
+
+// function saveToDos(){
+//     localStorage.setItem("todos", JSON.stringify(toDos));
+// }
+// // localStorage에는 텍스트만 저장할 수 있음. 배열을 저장할 수 없음
+// // 그러나 배열처럼 보이게 하기 -> JSON.stringify()
+// // 값을 string으로 저장하고 싶을 때 사용
+
+// function deleteToDo(event) {
+//     const li = event.target.parentElement;
+//     li.remove()
+// }
+
+// function paintToDo(newTodo) {
+//     const li = document.createElement("li")
+//     const span = document.createElement("span");
+//     span.innerText = newTodo;
+//     const button = document.createElement("button");
+//     button.innerText = "❌";
+//     button.addEventListener("click", deleteToDo)
+//     li.appendChild(span); 
+//     li.appendChild(button);
+//     toDoList.appendChild(li);
+// }
+
+// function handleToDoSubmit(event) {
+//     event.preventDefault();
+//     const newTodo = toDoInput.value;
+//     toDoInput.value = "";
+//     toDos.push(newTodo);
+//     paintToDo(newTodo);
+//     saveToDos();
+// }
+
+// toDoForm.addEventListener("submit", handleToDoSubmit) 
+
+
+
+// 7.4 Loading To Dos part one
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDos = [];
 
 function saveToDos(){
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
-// localStorage에는 텍스트만 저장할 수 있음. 배열을 저장할 수 없음
-// 그러나 배열처럼 보이게 하기 -> JSON.stringify()
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
@@ -115,3 +159,14 @@ function handleToDoSubmit(event) {
 
 toDoForm.addEventListener("submit", handleToDoSubmit) 
 
+function sayHello() {
+
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (saveToDos) {
+    const parsedToDos = JSON.parse(savedToDos);
+    // JSON.parse(saveToDos) : saveToDos를 배열로 바꿀 수 있음
+    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+}
