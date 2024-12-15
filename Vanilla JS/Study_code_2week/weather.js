06c51fa9a1f8dd29ac0df266b3f0b676468f1551
aaaange <ayung00@naver.com>
2024-12-16 01:57:12 +0900

    Vanilla JS day 14 - Geolocation

---
+++ Vanilla JS/Study_code_2week/weather.js
@@ -0,0 +1,14 @@
+// 8.0 Geolocation
+// geolocation : 사용자의 위치
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live in", lat, lng);
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError)
