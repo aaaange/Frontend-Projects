6fd8428bf6bd4b4205ca6be5c3a46b4999681166
aaaange <ayung00@naver.com>
2024-12-16 03:41:24 +0900

    Vanilla JS Final

---
+++ Vanilla JS/Final/js/todo.js
@@ -0,0 +1,67 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove()
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos()
+}
+
+// function checkToDo(event) {
+//     const button = event.target;
+//     button.innerText = "✔️";
+//     const li = event.target.parentElement;
+//     console.log(li)
+//     localStorage.getItem(parseInt(li.id));
+// }
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li")
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo)
+    // const checkButton = document.createElement("button");
+    // checkButton.innerText = "◽";
+    // checkButton.addEventListener('click', checkToDo)
+    li.appendChild(span); 
+    // li.appendChild(checkButton);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+        do: false,
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit) 
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos
+    parsedToDos.forEach(paintToDo);
+}
